fe330bad9b874f035e912e40111fe90c7ce406c3
psiha <psiha@users.noreply.github.com>
2023-05-08 12:10:48 +0000

    deploy: 1f38828292d830728ece26bf3e1b568e372f9d52

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['linear_5findex_0',['linear_index',['../group__utility_ga032b4c6695f7745bded81660a8975262.html#ga032b4c6695f7745bded81660a8975262',1,'kwk::linear_index(shape&lt; Shaper &gt; const &amp;sh, Index... idx) noexcept'],['../group__utility_ga453c1bba620b07a7e6e4185d7e89a31d.html#ga453c1bba620b07a7e6e4185d7e89a31d',1,'kwk::linear_index(shape&lt; Shaper &gt; const &amp;sh, Indexes idx) noexcept'],['../group__utility_gace68b8bfb1798dda6aea3e30ad00ec99.html#gace68b8bfb1798dda6aea3e30ad00ec99',1,'kwk::linear_index(shape&lt; Shaper &gt; const &amp;sh, Indexes const &amp;indexes) noexcept']]],
+  ['linear_5findex_0',['linear_index',['../group__utility_gae0b0eff30fd9bc044d032c5b6e114f2e.html#gae0b0eff30fd9bc044d032c5b6e114f2e',1,'kwk::linear_index(shape&lt; Shaper &gt; const sh, Index... idx) noexcept'],['../group__utility_ga453c1bba620b07a7e6e4185d7e89a31d.html#ga453c1bba620b07a7e6e4185d7e89a31d',1,'kwk::linear_index(shape&lt; Shaper &gt; const &amp;sh, Indexes idx) noexcept'],['../group__utility_gace68b8bfb1798dda6aea3e30ad00ec99.html#gace68b8bfb1798dda6aea3e30ad00ec99',1,'kwk::linear_index(shape&lt; Shaper &gt; const &amp;sh, Indexes const &amp;indexes) noexcept']]],
   ['linearize_1',['linearize',['../structkwk_1_1stride.html#a1ea311567df126bf74e70063044ce85f',1,'kwk::stride']]]
 ];
